19731c252f3a163fce5be88e2c51c0fec07f2531
NSavov <nedko066@gmail.com>
2017-02-21 13:18:21 +0100

    change denoise, in process of changing histeq

--- assignment 2/part 2/denoise.m
+++ assignment 2/part 2/denoise.m
@@ -1,30 +1,42 @@
+% function imOut = denoise(image, kernel_type, kernel_size)
+%     if strcmp(kernel_type, 'box')
+%         imOut = imboxfilt(image, kernel_size);
+%     end
+%     
+%     if strcmp(kernel_type, 'median')
+%         imOut = medfilt2(image, kernel_size);
+%     end
+% 
+% end
+
 function imOut = denoise(image, kernel_type, kernel_size)
-    if strcmp(kernel_type, 'box')
-        imOut = imboxfilt(image, kernel_size);
+    k_rows = kernel_size(1);
+    k_cols = kernel_size(2);
+    rows = size(image, 1);
+    cols = size(image, 2);
+    imOut = zeros([rows, cols], 'uint8');
+    r_dist = (k_rows-1)/2;
+    c_dist = (k_cols-1)/2;
+    for rowInd = r_dist+1:(rows-r_dist)
+        for colInd = c_dist+1:(cols-c_dist)
+            subM = image((rowInd - r_dist):(rowInd + r_dist), (colInd - c_dist):(colInd + c_dist));
+            if strcmp(kernel_type, 'box')
+                imOut(rowInd, colInd) = box_filter(subM);
+            end
+            
+            if strcmp(kernel_type , 'median')
+                imOut(rowInd, colInd) = median_filter(subM);
+            end
+        end
     end
     
-    if strcmp(kernel_type, 'median')
-        imOut = medfilt2(image, kernel_size);
-    end
-
+    imshow(imOut)
 end
-% 
-% function imOut = denoise(image, kernel_type, kernel_size)
-%     k_rows = kernel_size(1);
-%     k_cols = kernel_size(2);
-%     rows = size(image, 1);
-%     cols = size(image, 2);
-%     imOut = zeros(rows, cols);
-%     for rowInd = (k_rows-1)/2:(rows-(k_rows-1)/2)
-%         for colInd = (k_cols-1)/2:(cols-(k_cols-1)/2)
-%             subM = image(rowInd - (k_rows-1)/2, colInd - (k_cols-1)/2);
-%             if kernel_type = 'box'
-%                 
-%             end
-%         end
-%     end
-% end
-%     
-% function imOut = denoise(image, kernel_type, kernel_size)
-%     
-% end
+    
+function filterOut = box_filter(subM)
+    filterOut = mean(subM(:));
+end
+
+function filterOut = median_filter(subM)
+    filterOut = median(subM(:));
+end
--- assignment 2/part 2/my_HistMatching.m
+++ assignment 2/part 2/my_HistMatching.m
@@ -2,19 +2,67 @@ function imOut = my_HistMatching(input , reference )
     %Get histValues for each channel
     [i_frequencies, xi] = imhist(input);
     [r_frequencies, xr] = imhist(reference);
+    
     %Plot them together in one plot
     subplot(3, 2, 1);
     plot(xi, i_frequencies); %input histogram
+    title('input')
     subplot(3, 2, 2);
     imshow(input);
+    
     subplot(3, 2, 3);
     plot(xr, r_frequencies); %reference histogram
+    title('reference')
     subplot(3, 2, 4);
     imshow(reference);
+    
+    subplot(3, 2, 5);
     modified = histeq(reference, i_frequencies);
     [m_frequencies, xm] = imhist(modified);
-    subplot(3, 2, 5);
     plot(xm, m_frequencies);
+    title('modified')
     subplot(3, 2, 6);
     imshow(modified)
+end
+
+function equalized = hist_eq(input, reference)
+[nn,cum] = computeCumulativeHistogram(a,n);
+    T = createTransformationToIntensityImage(a,hgram,m,n,nn,cum);
+    b = grayxformmex(a, T);
+    
+cumd = cumsum(hgram*numel(a)/sum(hgram));
+
+% Create transformation to an intensity image by minimizing the error
+% between desired and actual cumulative histogram.
+tol = ones(m,1)*min([nn(1:n-1),0;0,nn(2:n)])/2;
+err = (cumd(:)*ones(1,n)-ones(m,1)*cum(:)')+tol;
+d = find(err < -numel(a)*sqrt(eps));
+if ~isempty(d)
+   err(d) = numel(a)*ones(size(d));
+end
+[dum,T] = min(err); %#ok
+T = (T-1)/(m-1);
+
+% Normalize hgram
+hgram = hgram*(numel(a)/sum(hgram));
+
+%     M = zeros(256,1,'uint8'); % Store mapping - Cast to uint8 to respect data type
+%     img1 = input;
+%     img2 = reference;
+%     
+% hist1 = imhist(img1);        % Compute histograms
+% hist2 = imhist(img2);
+% cdf1 = cumsum(hist1) / numel(img1);  %Compute CDFs
+% cdf2 = cumsum(hist2) / numel(img2);
+% 
+% % Compute the mapping
+% for idx = 1 : 256
+%     [~,ind] = min(abs(cdf1(idx) - cdf2));
+%     M(idx) = ind-1;
+% end
+% 
+% %Now apply the mapping to get first image to make
+% %the image look like the distribution of the second image
+% equalized = M(double(img1)+1);
+
 end
