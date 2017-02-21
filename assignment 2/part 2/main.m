7ed87079f25f442ade46dbe0d81b30dcdb2756c6
fabigato <grsmasher@gmail.com>
2017-02-21 20:27:16 +0100

    Merge branch 'master' of https://github.com/NSavov/CV-Assignments

--- assignment 2/part 2/main.m
+++
@@ -1,3 +0,0 @@
-lina_img = imread('../Images/image1.jpeg');
-moon_img = imread('../Images/image4.jpeg');
-%my_HistMatching(moon_img, lina_img)
--- assignment 2/part 2/my_HistMatching.m
+++ assignment 2/part 2/my_HistMatching.m
@@ -26,43 +26,52 @@ function imOut = my_HistMatching(input , reference )
 end
 
 function equalized = hist_eq(input, reference)
-[nn,cum] = computeCumulativeHistogram(a,n);
-    T = createTransformationToIntensityImage(a,hgram,m,n,nn,cum);
-    b = grayxformmex(a, T);
+    %get reference histogram
+    [hgram, ~] = imhist(reference);
+    m = length(hgram);
+    n=m;
     
-cumd = cumsum(hgram*numel(a)/sum(hgram));
+    %normalize hgram
+    hgram = hgram*(numel(input)/sum(hgram));
+    
+    %compute cummulative distribution of input
+    nn = imhist(input,numel(hgram))';
+    cummulative = cumsum(nn);
+    
+        
+    cumd = cumsum(hgram);
 
-% Create transformation to an intensity image by minimizing the error
-% between desired and actual cumulative histogram.
-tol = ones(m,1)*min([nn(1:n-1),0;0,nn(2:n)])/2;
-err = (cumd(:)*ones(1,n)-ones(m,1)*cum(:)')+tol;
-d = find(err < -numel(a)*sqrt(eps));
-if ~isempty(d)
-   err(d) = numel(a)*ones(size(d));
+    % Create transformation to an intensity image by minimizing the error
+    % between desired and actual cumulative histogram.
+    tol = ones(m,1)*min([nn(1:n-1),0;0,nn(2:n)])/2;
+    err = (cumd(:)*ones(1,n)-ones(m,1)*cummulative(:)')+tol;
+    d = find(err < -numel(input)*sqrt(eps));
+    if ~isempty(d)
+       err(d) = numel(input)*ones(size(d));
+    end
+    [dum,T] = min(err); %#ok
+    T = (T-1)/(m-1);
+    equalized = grayxformmex(input, T);
+
+
+
+M = zeros(256,1,'uint8'); % Store mapping - Cast to uint8 to respect data type
+img1 = input;
+img2 = reference;
+    
+hist1 = imhist(img1);        % Compute histograms
+hist2 = imhist(img2);
+cdf1 = cumsum(hist1) / numel(img1);  %Compute CDFs
+cdf2 = cumsum(hist2) / numel(img2);
+
+% Compute the mapping
+for idx = 1 : 256
+    [~,ind] = min(abs(cdf1(idx) - cdf2));
+    M(idx) = ind-1;
 end
-[dum,T] = min(err); %#ok
-T = (T-1)/(m-1);
 
-% Normalize hgram
-hgram = hgram*(numel(a)/sum(hgram));
-
-%     M = zeros(256,1,'uint8'); % Store mapping - Cast to uint8 to respect data type
-%     img1 = input;
-%     img2 = reference;
-%     
-% hist1 = imhist(img1);        % Compute histograms
-% hist2 = imhist(img2);
-% cdf1 = cumsum(hist1) / numel(img1);  %Compute CDFs
-% cdf2 = cumsum(hist2) / numel(img2);
-% 
-% % Compute the mapping
-% for idx = 1 : 256
-%     [~,ind] = min(abs(cdf1(idx) - cdf2));
-%     M(idx) = ind-1;
-% end
-% 
-% %Now apply the mapping to get first image to make
-% %the image look like the distribution of the second image
-% equalized = M(double(img1)+1);
+%Now apply the mapping to get first image to make
+%the image look like the distribution of the second image
+equalized = M(double(img1)+1);
 
 end
