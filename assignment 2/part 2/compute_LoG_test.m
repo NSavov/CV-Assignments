e78222ba1f2e727fedf12b8794dc5dcd12620ea7
fabigato <grsmasher@gmail.com>
2017-02-25 15:54:26 +0100

    "added normalization to imshow"

--- assignment 2/part 2/compute_LoG_test.m
+++ assignment 2/part 2/compute_LoG_test.m
@@ -1,7 +1,11 @@
 lena = imread('../Images/image1.jpeg');
 subplot(131)
-imshow(compute_LoG(lena, 1))
+% the [] parameter is a range specifying the black and white reference to
+% display the image. When the range is empty, then black and white will be
+% min(image(:)) and max(image(:)) respectively, so you can think of this as
+% a normalization
+imshow(compute_LoG(lena, 1), [])
 subplot(132)
-imshow(compute_LoG(lena, 2))
+imshow(compute_LoG(lena, 2), [])
 subplot(133)
-imshow(compute_LoG(lena, 3))
+imshow(compute_LoG(lena, 3), [])
