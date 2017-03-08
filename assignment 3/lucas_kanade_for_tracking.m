ef45675efff0e8ba68f88b4cfdd63c2f49794701
fabigato <grsmasher@gmail.com>
2017-03-08 16:07:58 +0100

    "renamed the file from points to tracking "

---
+++ assignment 3/lucas_kanade_for_tracking.m
@@ -0,0 +1,28 @@
+function [Vx, Vy] = lucas_kanade_for_tracking(image1, image2, window_size, c, r)
+    [Ix, Iy] = imgradientxy(image1);
+    sizex = size(image1, 1);
+    sizey = size(image1, 2);
+    It = double(image2) - double(image1);
+    Vx = [];
+    Vy = [];
+    for i = 1:size(c,2)
+            wx = r(i)-floor(window_size/2)+1;
+            wy = c(i)-floor(window_size/2)+1;
+            if wx<=0 || wy<=0 || (wx+window_size) > sizex || (wy+window_size) > sizey
+                Vx = [Vx 0];
+                Vy = [Vy 0];
+                continue
+            end
+            
+            region_Ix = Ix(wx:(wx+window_size), wy:(wy+window_size));
+            region_Iy = Iy(wx:(wx+window_size), wy:(wy+window_size));
+            region_It = It(wx:(wx+window_size), wy:(wy+window_size));
+            A = [region_Ix(:) region_Iy(:)];
+            b = -region_It(:);
+            %solution = linsolve(A, b);
+            solution = pinv(A)*b;
+            Vx = [Vx solution(1)];
+            Vy = [Vy solution(2)];
+            %iterate over the pixels in the window around the selected pixel
+    end
+end
