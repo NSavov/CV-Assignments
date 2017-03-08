158f89e48a16a5391cfbdf0e5b3647bcf80d5d95
fabigato <grsmasher@gmail.com>
2017-03-08 12:35:10 +0100

    "just added explanatory comments to the code"

--- assignment 3/harris.m
+++ assignment 3/harris.m
@@ -32,24 +32,38 @@ function [Ix, Iy, H, r, c] = harris(image, n, threshold, sigmaD, sigmaP)
     Ixy = Ix.*Iy;
     gkernel = fspecial('gaussian', [gauss_size gauss_size], sigmaP); % the autocorrelation
     % weights every pixel intensity with a gaussian distribution: that is,
-    % convolve the intensities with a gaussian kernel
+    % convolve the derivatives of the intensities with a gaussian kernel
     A = conv2( Ixx, gkernel, 'same');
+    % In theory A, B and C are scalars but since we'll have one of them for
+    % each pixel, we are saving them in this A, B and C matrices
     C = conv2(Iyy, gkernel, 'same');
     B = conv2(Ixy, gkernel,  'same');
 
     size_x = size(image, 1);
     size_y = size(image, 2);
     
+    % the cornerness formula: det(H) - 0.04*trace(H). In theory, H is a 2x2
+    % matrix, and we have one H per pixel, so we gather all of these such
+    % results into a single size_x by size_y H matrix
     H = (A.*C - B.*B) - 0.04*(A+C).*(A+C);
     
+    % now we'll use a trick: when there is a corner, several pixels can get
+    % a high H score, just because they all share the corner neighborhood.
+    % We want only one of them to be the corner. So we now explore each
+    % value of H to find out if its the biggest of its neighborhood. And 
+    % only so we check if it also passes the corner criteria.
     filter_window_size = 15;
     c = [];
     r = [];
     % H = H./max(max(H));
-    threshold = mean(H(H>0))*1.8
+    threshold = mean(H(H>0))*1.8;
+    %here we go, explore every value in H
     for x = 1:filter_window_size:size_x - filter_window_size
         for y = 1:filter_window_size:size_y - filter_window_size
+            % if the biggest value in the 'hood passes cornerness criteria
             if max(max(H(x:x+filter_window_size,y:y+filter_window_size))) > threshold
+                % then we gather its coordinates in c and r arrays. Not
+                % elegant at all but it works at least
                 [values, indexes] = max(H(x:x+filter_window_size,y:y+filter_window_size));
                 [~, index_x] = max(values);
                 corner_y = indexes(index_x);
