fbc9c939230879bf5f5829e5d878c987c4522f33
fabigato <grsmasher@gmail.com>
2017-03-10 11:02:39 +0100

    "modified match_features.m so that it only returns the SIFT artifacts and match_features_test uses them to plot"

--- assignment 4/match_features.m
+++ assignment 4/match_features.m
@@ -1,46 +1,19 @@
-original_img1 = imread('boat1.pgm');
-original_img2 = imread('boat2.pgm');
+% with code adapted from the official vlfeat tutorial at 
+% http://www.vlfeat.org/overview/sift.html
+function [matches, scores, f1, f2] = match_features(img1, img2)
 
-img1 = single(original_img1) ;
+    % get some image features (for boat1 it would be 2211). f1 will be a
+    % 4x2211 matrix (each column a feature frama of the form [X; Y; S;
+    % TH]), X,Y giving the fractional center of the frame, S is the scale
+    % and TH is the angle (in radians) indicating the orientation.
+    % For each frame in f1, the respective column in d1 is its descriptor
+    % as a 128x1 vector of [0,255] numbers
+    [f1,d1] = vl_sift(img1);
+    [f2,d2] = vl_sift(img2);
 
-[f1,d1] = vl_sift(img1) ;
-figure()
-imshow(original_img1);
-
-perm = randperm(size(f1,2)) ;
-sel = perm(1:50) ;
-h1 = vl_plotframe(f1(:,sel)) ;
-h2 = vl_plotframe(f1(:,sel)) ;
-set(h1,'color','k','linewidth',3) ; %draw black borders
-set(h2,'color','y','linewidth',2) ; %set yellow as the solid color
-
-
-figure() ; 
-
-side_view = cat(2, original_img1, original_img2);
-imshow(side_view) ;
-
-img2 = single(original_img2) ;
-[f2,d2] = vl_sift(img2) ;
-
-[matches, scores] = vl_ubcmatch(d1, d2) ;
-
-perm = randperm(size(matches,2)) ;
-sel = perm(1:50) ;
-
-x1 = f1(1,matches(1,sel)) ;
-x2 = f2(1,matches(2,sel)) + size(img1,2) ;
-y1 = f1(2,matches(1,sel)) ;
-y2 = f2(2,matches(2,sel)) ;
-
-hold on ;
-h = line([x1 ; x2], [y1 ; y2]) ;
-set(h,'linewidth', 1, 'color', 'b') ;
-
-
-vl_plotframe(f1(:,matches(1,sel))) ;
-f2(1,:) = f2(1,:) + size(img1,2) ;
-vl_plotframe(f2(:,matches(2,sel))) ;
-axis image off ;
-
-
+    % gets the matches found between the two sets of descriptors along with
+    % their match score (squared euclidean distance between the 2
+    % descriptors). if 1.5*squared_distance(Di, Dj) < squared_distance(Di,
+    % Dk) for every k != j, then Dj is considered a match for Di
+    [matches, scores] = vl_ubcmatch(d1, d2);
+end
--- assignment 4/match_features_test.m
+++ assignment 4/match_features_test.m
@@ -1,3 +1,31 @@
-image1 = 'boat1.pgm';
-image2= 'boat2.pgm';
-[matches, scores] = match_features(image1, image2);
+% prepare the images as single precision double matrices
+original_img1 = imread('boat1.pgm');
+original_img2 = imread('boat2.pgm');
+img1 = single(original_img1);
+img2 = single(original_img2);
+
+% get all the SIFT gadgets
+[matches, scores, f1, f2] = match_features(img1, img2);
+
+figure() ; 
+
+side_view = cat(2, original_img1, original_img2);
+imshow(side_view) ;
+
+perm = randperm(size(matches,2)) ;
+sel = perm(1:50) ;
+
+x1 = f1(1,matches(1,sel)) ;
+x2 = f2(1,matches(2,sel)) + size(img1,2) ;
+y1 = f1(2,matches(1,sel)) ;
+y2 = f2(2,matches(2,sel)) ;
+
+hold on ;
+h = line([x1 ; x2], [y1 ; y2]) ;
+set(h,'linewidth', 1, 'color', 'b') ;
+
+
+vl_plotframe(f1(:,matches(1,sel))) ;
+f2(1,:) = f2(1,:) + size(img1,2) ;
+vl_plotframe(f2(:,matches(2,sel))) ;
+axis image off ;
