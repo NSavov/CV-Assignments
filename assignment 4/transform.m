8a4e73b2cea7b8e8e3241c7fb9c8b1a81d08c947
fabigato <grsmasher@gmail.com>
2017-03-11 02:04:16 +0100

    "transoform.m works with rotation. Pending traslation"

--- assignment 4/transform.m
+++ assignment 4/transform.m
@@ -4,7 +4,7 @@ T = [cos(pi/4) -sin(pi/4) 0; sin(pi/4) cos(pi/4) 0; 100 50 1];
 % original_img1 = imread('boat1.pgm');
 % new_img1 = imwarp(original_img1, T_nice);
 % imshow(new_img1);
-im_test = zeros(10,10);
+im_test = zeros(10,10) + 0.5;
 im_test(5,:) = ones(1,10);
 figure();
 subplot(121);
@@ -17,30 +17,33 @@ title('original');
 corners = [1 1; original_h 1; 1 original_w; original_h original_w];
 
 newcorners = [corners(1,:)*T(1:2,1:2); corners(2,:)*T(1:2,1:2); corners(3,:)*T(1:2,1:2); corners(4,:)*T(1:2,1:2)];
-min_h = floor(min(newcorners(:,1)));% horizontal traslation factor for everybody
-if min_h > 0
-    min_h = 0;
+min_h = min(newcorners(:,1));% horizontal traslation factor for everybody
+offset_h = 0;
+if min_h < 1
+    offset_h = 1 + abs(min_h); 
 end
-min_w = floor(min(newcorners(:,2)));% vertical traslation factor for everybody
-if min_w > 0
-   min_w = 0; 
+min_w = min(newcorners(:,2));% vertical traslation factor for everybody
+offset_w = 0;
+if min_w < 1
+   offset_w = 1 + abs(min_w); 
 end
-newcorners
-newcorners(:,1) = newcorners(:,1) + abs(min_h) + 1;
-newcorners(:,2) = newcorners(:,2) + abs(min_w) + 1;
-new_h = max(floor(newcorners(:,1))) 
-new_w = max(floor(newcorners(:,2)))
-new_img = zeros(new_h, new_w)
-newcorners
-for y = 0:new_w-1
-   for x = 0:new_h-1
-       y
-       x
-       mapped_coords = T(1:2,1:2)'*[x; y]
-       mapped_coords = mapped_coords + [abs(min_h) + 1; abs(min_w) + 1]
-       mapped_coords = floor(mapped_coords);
-       new_img(x+1, y+1) = im_test(mapped_coords(1), mapped_coords(2))
+new_h = ceil(max(newcorners(:,1)) + offset_h);
+new_w = ceil(max(newcorners(:,2)) + offset_w);
+new_img = zeros(new_h, new_w);
+curated_newcorners = floor(newcorners) + [offset_h offset_w]
+onset_newcorners = curated_newcorners - [offset_h offset_w]
+inversed_transformed_newcorners = round(onset_newcorners*T(1:2,1:2)')
+for y = 1:new_w
+   for x = 1:new_h
+       mapped_coords = [x-offset_h y-offset_w]*T(1:2,1:2)';
+       mapped_coords = round(mapped_coords);
+       if mapped_coords(1) >= 1 && mapped_coords(1) <= original_h && mapped_coords(2) >= 1 && mapped_coords(2) <= original_w
+        new_img(x, y) = im_test(mapped_coords(1), mapped_coords(2));
+       end
    end
 end
+subplot(122);
+imshow(new_img);
+title('transformed');
 
 
