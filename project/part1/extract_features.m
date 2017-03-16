d2320d63475a6c5486d3585814bf52ae20dafa47
NSavov <nedko066@gmail.com>
2017-03-16 12:11:00 +0100

    opponent_sift

--- project/part1/extract_features.m
+++ project/part1/extract_features.m
@@ -1,7 +1,8 @@
-function [sift, RGB_sift, rgb_sift] = extract_features(image, method)
+function [sift, RGB_sift, rgb_sift, opponent_sift] = extract_features(image, method)
 grayscale_image = single(rgb2gray(image)) ;
 
 rgb_image = rgb2normedrgb(image);
+opponent_image = rgb2opponent(image);
 
 if strcmp(method, 'dense')
     [~,sift] = vl_dsift(grayscale_image) ;
@@ -14,6 +15,10 @@ if strcmp(method, 'dense')
     [~, r_sift] = vl_dsift(single(rgb_image(:,:,1)));
     [~, g_sift] = vl_dsift(single(rgb_image(:,:,2)));
     [~, b_sift] = vl_dsift(single(rgb_image(:,:,3)));
+    
+    [~, o1_sift] = vl_dsift(single(opponent_image(:,:,1)));
+    [~, o2_sift] = vl_dsift(single(opponent_image(:,:,2)));
+    [~, o3_sift] = vl_dsift(single(opponent_image(:,:,3)));
 elseif strcmp(method, 'keypoint')
     [~,sift] = vl_sift(grayscale_image) ;
     
@@ -25,8 +30,13 @@ elseif strcmp(method, 'keypoint')
     [~, r_sift] = vl_sift(single(rgb_image(:,:,1)));
     [~, g_sift] = vl_sift(single(rgb_image(:,:,2)));
     [~, b_sift] = vl_sift(single(rgb_image(:,:,3)));
+    
+    [~, o1_sift] = vl_sift(single(opponent_image(:,:,1)));
+    [~, o2_sift] = vl_sift(single(opponent_image(:,:,2)));
+    [~, o3_sift] = vl_sift(single(opponent_image(:,:,3)));
 end
 
 RGB_sift = cat(3, R_sift, G_sift, B_sift);
 rgb_sift = cat(3, r_sift, g_sift, b_sift);
+opponent_sift = cat(3, o1_sift, o2_sift, o3_sift);
 end
---
+++ project/part1/rgb2opponent.m
@@ -0,0 +1,12 @@
+function [output_image] = rgb2opponent(input_image)
+% converts an RGB image into opponent color space
+    r = input_image(:,:,1);
+    g = input_image(:,:,2);
+    b = input_image(:,:,3);
+
+    output_image  = input_image(:,:,:);
+    output_image(:,:,1) = (r - g)/sqrt(2);
+    output_image(:,:,2) = (r + g - 2*b)/sqrt(6);
+    output_image(:,:,3) = (r + g + b)/sqrt(3);
+end
+
