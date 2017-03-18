d467f41a660ac853b9d8b95b8173a3de4c9b1bdf
NSavov <nedko066@gmail.com>
2017-03-19 00:20:47 +0100

    SVM training and prediction

--- project/part1/predict_SVM.m
+++ project/part1/predict_SVM.m
@@ -8,24 +8,20 @@ category = 'airplanes_train';
 
 subset_size = 50;
 
-image_categories = string({'airplanes_test' 'cars_test' 'faces_test' 'motorbikes_test'});
+% image_categories = string({'airplanes_test' 'cars_test' 'faces_test' 'motorbikes_test'});
+image_categories = string({'airplanes_train' 'cars_train' 'faces_train' 'motorbikes_train'});
 histograms_dir = strcat(histograms_dir, feature_method ,'\', sift_type, '\histograms\');
 
-histograms = [];
+all_histograms = [];
 
-for category_ind = 1:1%size(image_categories, 2)
+for category_ind = 1:size(image_categories, 2)
     image_category = char(image_categories(category_ind));
     image_category
     image_category_dir = strcat(histograms_dir, image_category, '\');
-
-    files = dir(fullfile(image_category_dir, data_ext));
-
-    for i = 1:subset_size
-        load(strcat(image_category_dir, files(i).name), 'histogram');
-        histograms = cat(1, histograms, histogram);
-    end
+    load(strcat(histograms_dir, image_category, '.mat'), 'histograms');
+    all_histograms = cat(1, all_histograms, histograms(1:subset_size,:));
 end
 
-labels = cat(ones(subset_size, 1), zeros(3*subset_size, 1));
+labels = cat(1, ones(subset_size, 1), zeros(3*subset_size, 1));
 
- [predicted_label, accuracy, decision_values] = predict(labels, sparse(double(histograms)), model);
+ [predicted_label, accuracy, decision_values] = predict(labels, sparse(double(all_histograms)), model);
--- project/part1/train_SVM.m
+++ project/part1/train_SVM.m
@@ -7,22 +7,16 @@ category = 'airplanes_train';
 subset_size = 50;
 
 image_categories = string({'airplanes_train' 'cars_train' 'faces_train' 'motorbikes_train'});
-histograms_dir = strcat(histograms_dir, feature_method ,'\', sift_type, '\histograms\');
+histograms_dir = strcat(feature_dir, feature_method ,'\', sift_type, '\histograms\');
 
-histograms = [];
+all_histograms = [];
 
-for category_ind = 1:1%size(image_categories, 2)
+for category_ind = 1:size(image_categories, 2)
     image_category = char(image_categories(category_ind));
     image_category
-    image_category_dir = strcat(histograms_dir, image_category, '\');
-
-    files = dir(fullfile(image_category_dir, data_ext));
-
-    for i = 1:subset_size%size(files,1)
-        load(strcat(image_category_dir, files(i).name), 'histogram');
-        histograms = cat(1, histograms, histogram);
-    end
+    load(strcat(histograms_dir, image_category, '.mat'), 'histograms');
+    all_histograms = cat(1, all_histograms, histograms(1:subset_size,:));
 end
 labels = cat(1, ones(subset_size, 1), zeros(3*subset_size, 1));
 
-model = train(labels, sparse(double(histograms)));
+model = train(labels, sparse(double(all_histograms)), '-s 1');
