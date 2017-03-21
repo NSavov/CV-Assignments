55f84d777e33a17141829c0e8a674fb4c08388cc
fabigato <grsmasher@gmail.com>
2017-03-21 17:59:16 +0100

    "used the globals inside build_dataset"

--- project/part1/build_dataset.m
+++ project/part1/build_dataset.m
@@ -1,7 +1,6 @@
 globals;
 sift_method = 'keypoint';
 sift_type = 'grayscale';
-sample_size = 10;
 image_categories_train = string({'airplanes_train' 'cars_train' 'faces_train' 'motorbikes_train'});
 image_categories_test = string({'airplanes_test' 'cars_test' 'faces_test' 'motorbikes_test'});
 
@@ -20,13 +19,13 @@ build_histograms(sift_method, sift_type,image_categories_test, '_250','_400')
 
 % sift_method = 'keypoint';
 % sift_type = 'norm_rgb';
-% build_feature_set(sift_method, sift_type,image_categories_test, sample_size);
+% build_feature_set(sift_method, sift_type,image_categories_test, sift_step_size);
 % build_histograms(sift_method, sift_type,image_categories_train, '_250','_400')
 % build_histograms(sift_method, sift_type,image_categories_test, '_250','_400')
 % 
 % sift_method = 'keypoint';
 % sift_type = 'opponent';
-% build_feature_set(sift_method, sift_type,image_categories_test, sample_size);
+% build_feature_set(sift_method, sift_type,image_categories_test, sift_step_size);
 % build_histograms(sift_method, sift_type,image_categories_train, '_250','_400')
 % build_histograms(sift_method, sift_type,image_categories_test, '_250','_400')
 % 
@@ -47,49 +46,49 @@ build_histograms(sift_method, sift_type,image_categories_test, '_250','_400')
 
 % sift_method = 'dense';
 % sift_type = 'grayscale';
-% build_feature_set(sift_method, sift_type,image_categories_test, sample_size);
+% build_feature_set(sift_method, sift_type,image_categories_test, sift_step_size);
 % 
 % sift_method = 'dense';
 % sift_type = 'norm_rgb';
-% build_feature_set(sift_method, sift_type,image_categories_test, sample_size);
+% build_feature_set(sift_method, sift_type,image_categories_test, sift_step_size);
 % 
 % sift_method = 'dense';
 % sift_type = 'RGB';
-% build_feature_set(sift_method, sift_type,image_categories_test, sample_size);
+% build_feature_set(sift_method, sift_type,image_categories_test, sift_step_size);
 % 
 % sift_method = 'dense';
 % sift_type = 'opponent';
-% build_feature_set(sift_method, sift_type,image_categories_test, sample_size);
+% build_feature_set(sift_method, sift_type,image_categories_test, sift_step_size);
 % 
 % 
 % sift_method = 'keypoint';
 % sift_type = 'grayscale';
-% build_vocabulary(sift_method, sift_type, 800, 250, '800')
-% build_vocabulary(sift_method, sift_type, 1600, 250, '1600')
-% build_vocabulary(sift_method, sift_type, 2000, 250, '2000')
-% build_vocabulary(sift_method, sift_type, 4000, 250, '4000')
+% build_vocabulary(sift_method, sift_type, 800, vocabulary_fraction, '800')
+% build_vocabulary(sift_method, sift_type, 1600, vocabulary_fraction, '1600')
+% build_vocabulary(sift_method, sift_type, 2000, vocabulary_fraction, '2000')
+% build_vocabulary(sift_method, sift_type, 4000, vocabulary_fraction, '4000')
 
 % build_histograms(sift_method, sift_type,image_categories_train, '_250')
 % build_histograms(sift_method, sift_type,image_categories_test, '_250')
 
 
-% build_feature_set(sift_method, sift_type,image_categories_train, sample_size);
-% build_feature_set(sift_method, sift_type,image_categories_test, sample_size);
-% build_vocabulary(sift_method, sift_type, 400, 250)
+% build_feature_set(sift_method, sift_type,image_categories_train, sift_step_size);
+% build_feature_set(sift_method, sift_type,image_categories_test, sift_step_size);
+% build_vocabulary(sift_method, sift_type, 400, vocabulary_fraction)
 % 
-% build_vocabulary(sift_method, sift_type, 400, 250)
+% build_vocabulary(sift_method, sift_type, 400, vocabulary_fraction)
 % 
 % sift_method = 'dense';
 % sift_type = 'RGB';
-% build_vocabulary(sift_method, sift_type, 400, 250)
+% build_vocabulary(sift_method, sift_type, 400, vocabulary_fraction)
 % 
 % sift_method = 'dense';
 % sift_type = 'norm_rgb';
-% build_vocabulary(sift_method, sift_type, 400, 250)
+% build_vocabulary(sift_method, sift_type, 400, vocabulary_fraction)
 % 
 % sift_method = 'dense';
 % sift_type = 'opponent';
-% build_vocabulary(sift_method, sift_type, 400, 250)
+% build_vocabulary(sift_method, sift_type, 400, vocabulary_fraction)
 % 
 % sift_method = 'dense';
 % sift_type = 'grayscale';
@@ -114,13 +113,13 @@ build_histograms(sift_method, sift_type,image_categories_test, '_250','_400')
 % %dense grayscale bigger vocabularies
 % sift_method = 'dense';
 % sift_type = 'grayscale';
-% build_vocabulary(sift_method, sift_type, 800, 250, '800')
+% build_vocabulary(sift_method, sift_type, 800, vocabulary_fraction, '800')
 % 
 % sift_method = 'dense';
 % sift_type = 'grayscale';
-% build_vocabulary(sift_method, sift_type, 1600, 250, '1600')
+% build_vocabulary(sift_method, sift_type, 1600, vocabulary_fraction, '1600')
 % 
 % sift_method = 'dense';
 % sift_type = 'grayscale';
-% build_vocabulary(sift_method, sift_type, 2000, 250, '2000')
+% build_vocabulary(sift_method, sift_type, 2000, vocabulary_fraction, '2000')
 % 
--- project/part1/run_SVM.m
+++ project/part1/run_SVM.m
@@ -1,3 +1,4 @@
+globals;
 feature_methods = {'keypoint'};%, 'dense'};
 sift_types = {'grayscale', 'RGB', 'norm_rgb', 'opponent'};
 
@@ -13,7 +14,7 @@ for feature_method_i=1:1%size(feature_methods,2)
     feature_method = feature_methods{feature_method_i};
     for sift_type_i=1:1%size(sift_types,2)
         sift_type = sift_types{sift_type_i};
-        models = train_SVM(feature_method, sift_type, kernel, start, train_set_size,histogram_file_suffix);
+        models = train_SVM(feature_method, sift_type, svm_kernel, start, train_set_size,histogram_file_suffix);
         [map, ap, ranking, ranking_fileName] = predict_SVM(feature_method, sift_type, models, test_set_size,histogram_file_suffix);
         results{feature_method_i*sift_type_i} = [map, ap, ranking, ranking_fileName];
     end
